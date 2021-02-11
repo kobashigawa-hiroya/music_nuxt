71a0555dde0830c53dcbdce6edddcfecb503cd43
hiroya <hiroya19890721@gmail.com>
2021-02-11 23:08:17 +0900

    npm install toneインストール

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,54 @@
 <template>
-<main class="main">
-  <button class="pushBtn">PUSH</button>
-  </main>
+    <main class="main">
+        <button 
+            class="startBtn"
+            @click="appStart"
+            v-if="isStarted==false">
+            Start
+        </button>
+        <section
+            class="touchZone"
+            v-else>
+            &nbsp;
+        </section>
+    </main>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isStarted: false
+    }
+  },
+  methods: {
+    appStart() {
+      this.isStarted = true
+    }
+  }
 
 }
 </script>
 
 <style>
-.main {
-  width:100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.main{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
-.pushBtn {
-  padding: 10px 20px;
+
+.startBtn{
+    padding: 10px 20px;
+}
+
+.touchZone{
+  background: linear-gradient( 45deg, rgb(255,255,255),rgb(100,255,255) );
+  width: 100%;
+  min-width: 100%;
+  height: 100vh;
+  min-height: 100vh;
 }
 
 </style>
